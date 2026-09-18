84226257b333b64e3858c68ad168a798e75d8493
pablophdev <pablo.philimon.zuniga@gmail.com>
2026-09-18 19:17:07 -0300

    chore: se agrega AuthController para ir probando el metodo de register y se agrega configuracion de seguridad

---
+++ auth-service/src/main/java/com/pabloph/auth_service/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.pabloph.auth_service.controller;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pabloph.auth_service.dto.RegisterRequest;
+import com.pabloph.auth_service.service.AuthService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+    
+    @PostMapping("/register")
+    public ResponseEntity<Map<String, String>> register(
+            @Valid @RequestBody RegisterRequest request
+    ) {
+
+        authService.register(request);
+
+        return ResponseEntity.ok(
+                Map.of("message", "Usuario registrado correctamente")
+        );
+    }
+    
+}
